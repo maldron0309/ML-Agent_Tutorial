23fc200183f6351e5d9682aad5185fa659c03ba1
maldron0309 <bagjinhyeong640@gmail.com>
2024-04-25 03:50:39 +0900

    end

---
+++ Assets/Scripts/MoveToTargetAgent.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using System.Runtime.InteropServices;
+
+public class MoveToTargetAgent : Agent
+{
+
+    [SerializeField] private Transform env;
+    [SerializeField] private Transform target;
+    [SerializeField] private SpriteRenderer background;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-3.5f, -1.5f), Random.Range(-3.5f, 3.5f));
+        target.localPosition = new Vector3(Random.Range(1.5f, 3.5f), Random.Range(-3.5f, 3.5f));
+
+        env.rotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
+        transform.rotation = Quaternion.identity;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)   
+    {
+        sensor.AddObservation((Vector2)transform.localPosition);
+        sensor.AddObservation((Vector2)target.localPosition);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+
+        float movementSpeed = 5f;
+
+        transform.localPosition += new Vector3(moveX, moveY) * Time.deltaTime * movementSpeed;
+
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out Target target))
+        {
+            AddReward(10f);
+            background.color = Color.green;
+            EndEpisode();
+        }
+        else if (other.TryGetComponent(out Wall wall))
+        {
+            AddReward(-2f);
+            background.color = Color.red;
+            EndEpisode();
+        }
+    }
+}
